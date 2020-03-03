56deba00d17ed4bde1e71b1c9905eb44ceebe39c
takayuki goto <takayuki@idein.jp>
2020-03-03 18:25:19 +0900

    fix: fatal error: 'gnu/stubs-32.h' file not found
    x86_64/linux$ cargo build --target=arm-unknown-linux-gnueabihf
    ..
    /usr/include/x86_64-linux-gnu/gnu/stubs.h:7:11: fatal error: 'gnu/stubs-32.h' file not found
    /usr/include/x86_64-linux-gnu/gnu/stubs.h:7:11: fatal error: 'gnu/stubs-32.h' file not found, err: true
    thread 'main' panicked at 'Unable to generate bindings: ()', src/libcore/result.rs:1188:5
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace.

--- build.rs
+++ build.rs
@@ -7,10 +7,9 @@ fn main() {
     let libv4l = pkg_config::probe_library("libv4l2").unwrap();
 
     // Path to directories of C header
-    let include_dirs: Vec<PathBuf> =
-        env::var("LIBCLANG_INCLUDE_PATH")
-            .map(|path| vec![PathBuf::from(path)])
-            .unwrap_or_default();
+    let include_dirs: Vec<PathBuf> = env::var("LIBCLANG_INCLUDE_PATH")
+        .map(|path| vec![PathBuf::from(path)])
+        .unwrap_or_default();
 
     let include_args: Vec<_> = include_dirs
         .iter()
@@ -18,8 +17,15 @@ fn main() {
         .flat_map(|path| vec!["-I", path.to_str().unwrap()])
         .collect();
 
+    let target_args = if let Some(target) = env::var("TARGET_ARCH").ok() {
+        vec!["-target".into(), target]
+    } else {
+        vec![]
+    };
+
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_args(&target_args)
         .clang_args(&include_args)
         .generate()
         .expect("Unable to generate bindings");
